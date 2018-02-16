ec5721500ca963b8316a197c9117435593a32139
Brian Ho <ho_brian@me.com>
2018-02-16 02:32:09 -0500

    Heatmap placeholder.

--- docs/js/visualizations/vis-map.js
+++ docs/js/visualizations/vis-map.js
@@ -8,13 +8,91 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(mymap);
 
 /*
+var svg = d3.select(mymap.getPanes().overlayPane).append("svg");
+var g = svg.append("g").attr("class", "leaflet-zoom-hide");
+
+function projectPoint(lat, lng) {
+    return mymap.latLngToLayerPoint(new L.LatLng(lat, lng));
+}
+
+function projectStream(lat, lng) {
+    var point = projectPoint(lat,lng);
+    this.stream.point(point.x, point.y);
+}
+
+var transform = d3.geoTransform({point: projectStream});
+var path = d3.geoPath().projection(transform);
+*/
+
+color = d3.scaleSequential(d3.interpolateGreens).domain([1,10]);
+      // .interpolate(d3.interpolateHcl)
+      // .range([d3.rgb("#007AFF"), d3.rgb('#FFF500')]);
+
 d3.queue()
   .defer(d3.json, "./data/map/2640_dissolve.geojson")
   .defer(d3.json, "./data/map/5280_dissolve.geojson")
+  .defer(d3.json, "./data/map/heatmap_after_grid.geojson")
+  .defer(d3.json, "./data/map/heatmap_before_grid.geojson")
   .await(makeMyMap);
+
+function makeMyMap(error, half, mile, grid, after, before){
+  if (error) throw error;
+
+  L.geoJSON(after, {
+    style: function(feature) {
+      if (feature.properties.count == 0) { return {opacity: 0, fillOpacity: 0};}
+      else { return {fillColor : color(feature.properties.count), fillOpacity: .8, opacity: 0, color: color(feature.properties.count)}; };
+
+      }
+  }).addTo(mymap);
+
+  var outlineStyle = {
+      "fillOpacity": 0,
+      "weight": 2,
+      "color": "black"
+  };
+
+  L.geoJSON(half, {style: outlineStyle}).addTo(mymap);
+  L.geoJSON(mile, {style: outlineStyle}).addTo(mymap);
+  // L.geoJSON(before).addTo(mymap);
+
+/*
+  var rectangles = g.selectAll("rectangle").data(grid);
+
+  rectangles.enter()
+    .append("rectangle")
+    .attr("height", 5)
+    .attr("width", 5)
+    .attr("x", function(d) { return projectPoint(d.Latitude_round, d.Longitude_round).x; })
+    .attr("y", function(d) { return projectPoint(d.Latitude_round, d.Longitude_round).y; });
 */
+};
+
+/*
+function update() {
+  console.log("update");
+    var bounds = path.bounds(grid),
+        topLeft = bounds[0],
+        bottomRight = bounds[1];
+
+    var buffer = 50;
+
+    svg .attr("width", bottomRight[0] - topLeft[0] + (buffer * 2))
+        .attr("height", bottomRight[1] - topLeft[1] + (buffer * 2))
+        .style("left", (topLeft[0] - buffer) + "px")
+        .style("top", (topLeft[1] - buffer) + "px");
+
+    g   .attr("transform", "translate(" + (-topLeft[0] + buffer) + "," + (-topLeft[1] + buffer) + ")");
+
+    circles
+        .attr("x", function(d) { return projectPoint(d.Latitude_round, d.Longitude_round).x; })
+        .attr("y", function(d) { return projectPoint(d.Latitude_round, d.Longitude_round).y; });
+};
+
+update();
+mymap.on("viewreset", update);
 
 d3.json('./data/map/2640_dissolve.geojson', function(data) {
   console.log(data);
-  L.geoJSON(data).addTo(mymap);
 });
+*/
